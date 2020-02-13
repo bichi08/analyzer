b18971e093d9ecd356f27742c98a0dbfdce9ebf0
bichi08 <33770822+bichi08@users.noreply.github.com>
2020-02-13 22:26:20 +0530

    Initial commit. Only imports.

---
+++ trial.py
@@ -0,0 +1,10 @@
+__author__ = "bichitra.satapathy"
+
+import numpy as np
+import pandas as pd
+
+"""
+This is a NLP application.
+We will first read the text and summarise it.
+Sentiment analysis is a must.
+"""
